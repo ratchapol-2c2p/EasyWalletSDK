433e3d107512137e98e08049383179e134fba3d1
Ratchapol Pattarakanoksiri <ratchapol.pat@2c2p.com>
2024-05-29 13:46:12 +0700

    Initial Commit

---
+++ EasyWalletSDK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyWalletSDK
+//
+//  Created by Ratchapol Pattarakanoksiri on 29/5/2567 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
